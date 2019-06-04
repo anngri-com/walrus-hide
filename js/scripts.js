d4432870839a037b3caf4443c61719532d31309d
Anna Gritsai and Samantha Priester <samantha@>
2019-06-04 10:55:33 -0700

    tried fade in and fade out jquery function

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   $(".clickable").click(function() {
-    $("#walrus-showing").toggle();
-    $("#walrus-hidden").toggle();
+    $("#walrus-showing").fadeIn(1000);
+  });
+  $(".clickable").click(function() {
+    $("#walrus-showing").fadeOut(100);
   });
 });
