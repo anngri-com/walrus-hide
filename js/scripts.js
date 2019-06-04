c7045fc55cef8e70f965fd3823fe3723fe101440
Anna Gritsai and Samantha Priester <anna@>
2019-06-04 11:39:52 -0700

    practiced to change background color with jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,25 @@ $(document).ready(function() {
     $("#walrus-showing").fadeIn(1000);
   });
   $(".clickable").click(function() {
-    $("#walrus-showing").fadeOut(100);
+    $("#walrus-showing").fadeOut(3000);
+  });
+
+/* Button function starts here */
+
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background");
+    // $("p").css("color", "white");
+    // $("h1").css("color", "white");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
   });
 });
